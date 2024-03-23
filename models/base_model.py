5b7252b69c03c4318378ee0a65218e554ce223ff
yam1er <mantynouyacoubou@gmail.com>
2024-03-22 17:06:00 -0700

    Create BaseModel from dictionary

--- models/base_model.py
+++ models/base_model.py
@@ -9,13 +9,24 @@ class BaseModel:
     """
       BaseModel Class
     """
-    def __init__(self):
+    def __init__(self, obj, *args, **kwargs):
         """
           Instance Creation Method
         """
         self.id = str(uuid.uuid4())
         self.created_at = datetime.datetime.now()
         self.updated_at = datetime.datetime.now()
+
+        if len(kwargs):
+            iso_format = "%Y-%m-%dT%H:%M:%S.%f"
+            for key, value in kwargs.items():
+                if key in ["created_at", "updated_at"]:
+                    self.__dict__[key] = datetime.strptime(value, iso_format)
+                else:
+                    self.__dict__[key] = value
+
+        else:
+            self.{}[f"{obj.__class__.__name__}.{obj.id}"] = obj
         
     def __str__(self):
         """
