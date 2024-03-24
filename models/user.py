6d5e2896521b92fbe1ee7e6b0691f31a8db0b34e
yam1er <mantynouyacoubou@gmail.com>
2024-03-24 16:14:53 -0700

    user class

---
+++ models/user.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+    Module for user class
+"""
+from models.base_module import BaseModel
+
+class User (BaseModel):
+    """
+    class that represent a user
+
+    Attributes;
+    email: string
+    password: string
+    first_name: string
+    last_name: string
+    """
+
+    email = ""
+    password = ""
+    first_name = ""
+    last_name = ""
