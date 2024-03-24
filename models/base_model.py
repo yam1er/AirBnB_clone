b1f64fb3592121b0ea09980584a468117ff77fe3
AlphonseMehounme <alphonsemehounme9@gmail.com>
2024-03-24 11:27:57 -0700

    Update to_dict() method

--- models/base_model.py
+++ models/base_model.py
@@ -18,7 +18,6 @@ class BaseModel:
         if len(kwargs):
             iso_format = "%Y-%m-%dT%H:%M:%S.%f"
             for key, value in kwargs.items():
-                print(key, value)
                 if key in ["created_at", "updated_at"]:
                     self.__dict__[key] = datetime.strptime(value, iso_format)
                 elif key != "__class__":
@@ -27,7 +26,7 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-            storage.new(self.to_dict())
+            storage.new(self)
 
     def __str__(self):
         """
@@ -46,8 +45,8 @@ class BaseModel:
         """
           To Dict method
         """
-        dict = self.__dict__
+        dict = self.__dict__.copy()
         dict['created_at'] = self.created_at.isoformat()
         dict['updated_at'] = self.updated_at.isoformat()
-        dict.update({'__class__': self.__class__.__name__})
+        dict['__class__'] = self.__class__.__name__
         return dict
