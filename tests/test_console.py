82a96c0af57ca6f10c2f31951a78f580892ed648
AlphonseMehounme <alphonsemehounme9@gmail.com>
2024-03-26 18:31:21 -0700

    Test

---
+++ tests/test_console.py
@@ -0,0 +1,20 @@
+"""
+  Test for base_model module
+"""
+import unittest
+from models.base_model import BaseModel
+
+
+class TestConsole(unittest.TestCase):
+    """
+      Test Console
+    """
+    def test_equal_instance(self):
+        """
+          Equal instance
+        """
+        mod = BaseModel()
+        self.assertEqual(True, 1)
+
+if __name__ == '__main__':
+    unittest.main()
